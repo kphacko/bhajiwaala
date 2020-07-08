d761943a07630bbd2e85845513d59bcaa522dce5
Ambar11 <ambarpatil1@gmail.com>
2020-07-08 16:47:32 +0530

    create order page done

--- bin/routes/order.js
+++ bin/routes/order.js
@@ -4,9 +4,11 @@ const userController = require('../controller/order');
 
 //some action to get products
 router.get('/getOrder/:id', userController.getOrder);
-
 //some action to add product
 router.post('/addOrder', userController.addOrder);
+router.get('/addOrder', (req, res) => {
+    res.render('createOrder');
+})
 
 //some action to edit product
 // router.patch('/editOrder', userController.editOrder);
