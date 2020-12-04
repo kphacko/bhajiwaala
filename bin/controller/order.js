1ee044386eee4469d80e409d59cf858ccf11bbc5
kphacko <karan2000patil@gmail.com>
2020-12-04 12:01:23 +0000

    daily summary change done 1/2

--- bin/controller/order.js
+++ bin/controller/order.js
@@ -1146,9 +1146,9 @@ exports.getDailySumByDate = async(date)=>{
          HotelOrders = await functions.querySingle(`SELECT invoice.paid_amount,orders.date,ordered_products.order_id,hotel.id AS hotel_id,hotel.name AS hotel_name,ordered_products.p_id,products.name,products.marathi,products.hindi,products.weight_type,ordered_products.quantity AS quantity ,SUM(ordered_products.price)  AS price,ordered_products.PerPrice FROM orders INNER JOIN ordered_products ON orders.id = ordered_products.order_id INNER JOIN hotel ON orders.ref_id = hotel.id INNER JOIN products ON ordered_products.p_id = products.id  INNER JOIN invoice ON invoice.ref_id = orders.id WHERE orders.date = '${date}' AND orders.status =0 AND  invoice.type = 0 AND orders.type='HOTEL' GROUP BY hotel.name`);        
   
          VendorOrders = await functions.querySingle(`SELECT invoice.paid_amount,orders.date,ordered_products.order_id,vendor.id AS vendor_id,vendor.name 
-         AS vendor_name,ordered_products.p_id,products.name,products.marathi,products.hindi,products.weight_type,ordered_products.quantity AS quantity ,SUM(ordered_products.price) AS price,ordered_products.PerPrice  FROM orders 
+         AS vendor_name,ordered_products.p_id,products.name,products.marathi,products.hindi,products.weight_type,ordered_products.quantity AS quantity ,ordered_products.price AS price,ordered_products.PerPrice  FROM orders 
          INNER JOIN ordered_products ON orders.id = ordered_products.order_id 
-         INNER JOIN vendor ON orders.ref_id = vendor.id INNER JOIN products ON ordered_products.p_id = products.id  INNER JOIN invoice ON invoice.ref_id = orders.id  WHERE orders.date = '${date}' AND invoice.type = 1 AND orders.status =0 AND orders.type='VENDOR' GROUP BY vendor.name`);        
+         INNER JOIN vendor ON orders.ref_id = vendor.id INNER JOIN products ON ordered_products.p_id = products.id  INNER JOIN invoice ON invoice.ref_id = orders.id  WHERE orders.date = '${date}' AND invoice.type = 1 AND orders.status =0 AND orders.type='VENDOR'`);        
          expense = await functions.querySingle(`SELECT * FROM expense WHERE status = 0 AND date = '${date}' `);
         //  console.log(VendorOrders);
        
